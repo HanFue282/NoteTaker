c90dc33e0097508cc9cad87ecd9543f392ed0152
HanFue282 <59034654+HanFue282@users.noreply.github.com>
2020-02-23 15:29:33 -0600

    adjusting DELETE express

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -11,24 +11,18 @@ module.exports = function(app) {
 
     noteData.push(newNote);
     res.json(noteData);
-    return noteData;
   });
 
   app.delete('/api/notes/:id', function (req, res) {
-    console.log(noteData.length);
     var loc = 0;
 
-    for(var i = 0; i < noteData.length; i++)
-    {
+    for(var i = 0; i < noteData.length; i++){
       if(noteData[i].id == req.params.id)
       {console.log(noteData[i]);
         loc = i;
-        console.log(loc);
       }
     }
-
-    noteData.splice(loc, 1)
-    fs.writeFileAsync("../db/db.json", JSON.stringify(noteData));
-
+      noteData.splice(loc, 1);
+      fs.writeFileSync("../db/db.json", "public", JSON.stringify(noteData));
   });
 };
