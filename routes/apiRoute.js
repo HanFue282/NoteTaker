17fc25d7381be36cdc4452f144dd52f6a473949b
HanFue282 <59034654+HanFue282@users.noreply.github.com>
2020-02-22 18:50:51 -0600

    saving point, app POST functionality is working

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,14 +1,14 @@
 var noteData = require("../db/db");
 
-
 module.exports = function(app) {
-  // API GET Requests
-
   app.get("/api/notes", function(req, res) {
     res.json(noteData);
   });
 
   app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+
+    noteData.push(newNote);
     res.json(noteData);
   });
 
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 
 // Express use for localhost number
 var app = express();
-
 var PORT = process.env.PORT || 3006;
 
 app.use(express.urlencoded({ extended: true }));
